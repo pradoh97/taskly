5695de5a0270f1528ffb6c0c0d5c20d11e35299e
pradoh97 <pradoh.97@gmail.com>
2020-08-07 18:43:18 -0300

    Edicion de tareas y alternado de estados completo e incompleto

--- js/modules/tareas.js
+++ js/modules/tareas.js
@@ -8,12 +8,21 @@ class Tarea {
   }
   mostrarTarea(){
     let atributoTarea, estado;
+    let tituloPlaceholder = '';
+    let descripcionPlaceholder = '';
+
+    if(this.titulo == 'No te olvides de completarme :('){
+      tituloPlaceholder = 'placeholder';
+    }
+    if(this.descripcion == 'No te olvides de completarme :('){
+      descripcionPlaceholder = 'placeholder';
+    }
     this.nodo = document.createElement('div');
     this.nodo.classList.add('tarea');
     this.nodo.innerHTML = `
 
     <div class="titulo">
-    <h2 class="campo-editable">${this.titulo}</h2><i class="fas fa-pen"></i>
+    <h2 class="campo-editable ${tituloPlaceholder}">${this.titulo}</h2><i class="fas fa-pen"></i>
     </div>
     <div class="descripcion">
     <p class="campo-editable">
@@ -27,14 +36,18 @@ class Tarea {
       <button type="button">
         <i class="fas fa-check"></i> Marcar como incompleta.
       </button>`;
+      this.nodo.querySelector('button').addEventListener('click', alternarTarea);
     } else {
       this.nodo.innerHTML += `
       <button class="exito" type="button">
         <i class="fas fa-check"></i> Marcar como completa.
       </button>
       `
-      this.nodo.querySelector('.exito').addEventListener('click', completarTarea);
+      this.nodo.querySelector('.exito').addEventListener('click', alternarTarea);
     }
+
+    this.nodo.querySelector('h2.campo-editable').addEventListener('click', editarTarea);
+    this.nodo.querySelector('p.campo-editable').addEventListener('click', editarTarea);
     atributoTarea = document.createAttribute('data-id-tarea');
     atributoTarea.value = this.id;
 
@@ -114,29 +127,87 @@ function cargarTareas(){
     }
   }
 }
+function alternarTarea(e){
+  let boton = e.target;
+  let padre = boton;
+  let estado = '';
 
-function completarTarea(e){
-  let nodo = e.target;
-  nodo.classList.remove('exito');
-  nodo.innerHTML = `<i class="fas fa-check"></i> Marcar como incompleta.`;
+  while(!padre.classList.contains('tarea')){
+    padre = padre.parentElement;
+  }
+
+  if(boton.classList.contains('exito')){
+    boton.classList.remove('exito');
+    boton.innerHTML = `<i class="fas fa-check"></i> Marcar como incompleta.`;
+    estado = 'completa';
+    padre.classList.add(estado);
+  } else {
+    boton.classList.add('exito');
+    boton.innerHTML = `<i class="fas fa-check"></i> Marcar como completa.`;
+    padre.classList.remove('completa')
+  }
   let claveLS = 'tarea-';
   let tarea;
 
-
-  while(!nodo.classList.contains('tarea')){
-    nodo = nodo.parentElement;
-  }
-  nodo.classList.add('completa');
-  claveLS += nodo.dataset.idTarea;
+  claveLS += padre.dataset.idTarea;
 
   for(let i = 0; i<localStorage.length; i++){
     if(localStorage.key(i) == claveLS){
       tarea = localStorage.getItem(claveLS);
       tarea = JSON.parse(tarea);
-      tarea.estado = 'completa';
+      tarea.estado = estado;
       localStorage.setItem(claveLS, JSON.stringify(tarea));
     }
   }
 
   contarTareas();
+
+}
+
+function editarTarea(e){
+  let texto = e.target.innerText;
+  let input = document.createElement('input');
+  let contenedor = e.target.parentElement;
+  contenedor.insertBefore(input,e.target);
+  contenedor.removeChild(e.target);
+  input.value = texto;
+  input.select();
+  input.addEventListener('keyup', guardarCambiosTarea);
+  input.addEventListener('blur', guardarCambiosTarea);
+}
+
+function guardarCambiosTarea(e){
+  let texto = e.target.value;
+  let esPlaceHolder = false;
+  if(!texto || texto == ''){
+    texto = 'No te olvides de completarme :('
+    esPlaceHolder = true;
+  }
+  let tarea = e.target;
+  let tipoDato = tarea.parentElement;
+
+  if(e.keyCode == 13 || e.which == 13 || e.type == 'blur'){
+    let idTarea = e.target.parentElement.parentElement.dataset.idTarea;
+    let tareaOriginal = JSON.parse(localStorage.getItem('tarea-' + idTarea));
+    if(tarea.parentElement.classList.contains('descripcion')){
+      tareaOriginal.descripcion = texto;
+    } else if (tarea.parentElement.classList.contains('titulo')){
+      tareaOriginal.titulo = texto;
+    }
+    guardarLocalStorage('tarea-' + tareaOriginal.id, tareaOriginal);
+    let elemento;
+    if(tipoDato.classList.contains('titulo')){
+      elemento = document.createElement('h2');
+    } else if (tipoDato.classList.contains('descripcion')){
+      elemento = document.createElement('p');
+    }
+    elemento.classList.add('campo-editable');
+    if(esPlaceHolder){
+      elemento.classList.add('placeholder');
+    }
+    elemento.innerText = texto;
+    elemento.addEventListener('click', editarTarea);
+    tipoDato.insertBefore(elemento, tarea);
+    tipoDato.removeChild(tarea);
+  }
 }
