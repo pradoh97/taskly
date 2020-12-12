4f5c293a970864d359cd75e00d09805100284e07
Hernan <pradoh97@gmail.com>
2020-12-12 11:36:32 -0300

    Agrego tareas de prueba

--- js/app.js
+++ js/app.js
@@ -17,4 +17,15 @@ window.addEventListener('load', iniciarApp);
 
 function iniciarApp(){
   Tarea.cargarTareas();
+
+/*
+  let tareas = [];
+  tareas.push(new Tarea(1, "a", "a"));
+  tareas.push(new Tarea(2, "b", "b"));
+  tareas.push(new Tarea(3, "c", "c"));
+  tareas.push(new Tarea(4, "d", "d"));
+  tareas.push(new Tarea(5, "e", "e"));
+
+  for(let tarea of tareas) tarea.cargar();
+  */
 }
