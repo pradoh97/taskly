dbf36e67ce3b64a1ba6d6e0bda790252ea214b80
Hernan <pradoh97@gmail.com>
2020-12-16 16:25:11 -0300

    Agregadas opciones para modals y eventos para navegar con tab.

--- js/app.js
+++ js/app.js
@@ -1,30 +1,36 @@
 const tableroPorDefecto = document.getElementById('tablero-principal');
 
-const opciones = [];
+const opcionesTarea = [];
+const opcionesModal = [];
+let ultimaTareaActiva;
 
-opciones['completar'] = {
+opcionesTarea['completar'] = {
   habilitado: true,
   texto: 'Completar',
   icono: null
 }
-opciones['eliminar'] = {
+opcionesTarea['eliminar'] = {
   habilitado: true,
   texto: 'Eliminar',
   icono: null
 }
 
-window.addEventListener('load', iniciarApp);
+opcionesModal['crear tarea'] = {
+  habilitado: true,
+  texto: 'Crear tarea',
+  icono: null
+}
+opcionesModal['cancelar'] = {
+  habilitado: true,
+  texto: 'Cancelar',
+  icono: null
+}
 
+
+window.addEventListener('load', iniciarApp);
 function iniciarApp(){
   Tarea.cargarTareas();
 
-  // let tareas = [];
-  // tareas.push(new Tarea(1, "a", "a"));
-  // tareas.push(new Tarea(2, "b", "b"));
-  // tareas.push(new Tarea(3, "c", "c"));
-  // tareas.push(new Tarea(4, "d", "d"));
-  // tareas.push(new Tarea(5, "e", "e"));
-  //
-  // for(let tarea of tareas) tarea.cargar();
-
+  document.addEventListener('keyup', Utils.mostrarOpcionesTarea);
+  document.addEventListener('click', Utils.mostrarOpcionesTarea);
 }
