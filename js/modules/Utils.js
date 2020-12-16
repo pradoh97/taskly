41cf1df193426178b3a128d939ed81dbea1002fd
Hernan <pradoh97@gmail.com>
2020-12-16 17:09:06 -0300

    Agregada navegación por tab y mostrar opciones en tareas con tab. Agregados métodos para manipular backdrop. Agregado método para crear opciones en tareas/modals

--- js/modules/Utils.js
+++ js/modules/Utils.js
@@ -45,13 +45,34 @@ class Utils {
     let tarea;
     tarea = Tarea.obtenerTarea(null, e.target);
 
+    if(e.type == 'input'){
+      if(e.target.localName == 'h2'.toLowerCase()) tarea.LS.cambiarTitulo(e.target.innerText);
+      if(e.target.localName == 'p'.toLowerCase()) tarea.LS.cambiarDescripcion(e.target.innerText);
+    }
+
+    if(e.type == 'click'){
+      switch (e.target.dataset.accion) {
+        case "completar":
+          tarea.LS.alternarEstado()
+          break;
+        case "eliminar":
+          tarea.LS.eliminar();
+          break;
+      }
+    }
+  }
+
+  static eventoModal(e){
+    let modal;
+    modal = Modal.obtenerModal();
+
     switch (e.target.dataset.accion) {
-      case "completar":
-        tarea.LS.alternarEstado()
-        break;
-      case "eliminar":
-        tarea.LS.eliminar();
-        break;
+      case "crear tarea":
+        let tituloTarea = modal.DOM.querySelector('input').value;
+        let tarea = new Tarea(Tarea.idUltimaTarea() + 1, tituloTarea);
+        tarea.cargar();
+      default:
+        modal.objeto.ocultar();
     }
   }
 
@@ -88,6 +109,65 @@ class Utils {
   static agregarPrefijoTarea(id, esTarea){
     if(esTarea) id = 'tarea-' + id;
     return id;
+  }
+
+  static eliminarBackdrop(){
+    document.querySelector('main').removeChild(document.querySelector('.backdrop'));
+  }
+
+  static agregarBackdrop(){
+    let backdrop = document.createElement('div');
+    backdrop.classList.add('backdrop');
+    document.querySelector('main').appendChild(backdrop);
+  }
+
+  //Recibe una lista de opciones, un elemento al cual agregar cada opción
+  //y el objeto al que corresponde (para disparar los eventos correctos).
+  //Se usa para agregar botones con acciones que el usuario puede ejecutar.
+  static crearOpciones(listaOpciones, elemento, objeto){
+
+    for(let opcion in listaOpciones){
+      let boton = document.createElement('button');
+
+      boton.type="button";
+
+      //Con el texto que está en el objeto del array opciones.
+      boton.innerText = listaOpciones[opcion].texto;
+
+      //Defino la acción que realiza el botón. Esta después es útil para cuando se
+      //clickea en el botón.
+      boton.dataset.accion = opcion;
+
+      //El método eventoX se encarga de definir que sucede al clickear
+      //una opción de un boton en base a qué opcion (su data-accion) disparó el evento.
+      if(objeto === 'tarea') boton.addEventListener('click', Utils.eventoTarea);
+      else boton.addEventListener('click', Utils.eventoModal);
+
+      //Si hay icono, lo agrego.
+      if(listaOpciones[opcion].icono) boton.innerText += listaOpciones[opcion].icono;
+
+      elemento.appendChild(boton);
+    }
+  }
+
+  static mostrarOpcionesTarea(e){
+    if(e.target.localName.toLowerCase() == 'body' || e.target.localName.toLowerCase() == 'html'){
+      if(ultimaTareaActiva){
+        ultimaTareaActiva.querySelector('.tarea__opciones').classList.remove('visible');
+      }
+      return;
+    }
+
+    let tarea = Tarea.obtenerTarea(null, e.target);
+
+    if (e.keyCode == 9 || e.type == 'click') {
+      if(tarea && tarea.DOM){
+        if(tarea != ultimaTareaActiva && ultimaTareaActiva) ultimaTareaActiva.querySelector('.tarea__opciones').classList.remove('visible');
+
+        ultimaTareaActiva = tarea.DOM;
+        ultimaTareaActiva.querySelector('.tarea__opciones').classList.add('visible');
+      }
+    }
   }
 
 }
